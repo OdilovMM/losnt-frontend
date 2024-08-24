103d26d0509f661c900c6ea87400d719b02bfa12
OdilovMM <dssrinman@gmail.com>
2024-08-24 18:41:05 +0500

    rebux

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as FormInput } from "./FormInput";
-export { default as Footer } from "./Footer";
-export { default as Header } from "./Header";
-export { default as ItemsGrid } from "./ItemsGrid";
-export { default as SubmitBtn } from "./SubmitBtn";
-export { default as Filters } from "./Filters.jsx";
-export { default as Pagination } from "./Pagination";
---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+export { default as FormInput } from "./FormInput.jsx";
+export { default as Footer } from "./Footer.jsx";
+export { default as Header } from "./Header.jsx";
+export { default as ItemsGrid } from "./ItemsGrid.jsx";
+export { default as SubmitBtn } from "./SubmitBtn.jsx";
+export { default as Filters } from "./Filters.jsx";
+export { default as Pagination } from "./Pagination.jsx";
