ad0ccc713b6c3bd3ff1ac02fe23a0878f1bc5226
OdilovMM <dssrinman@gmail.com>
2024-08-24 18:36:53 +0500

    reinstall#2

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as ItemsGrid } from "./ItemsGrid";
 export { default as SubmitBtn } from "./SubmitBtn";
-export { default as Filters } from "./Filters";
+export { default as Filters } from "./Filters.jsx";
 export { default as Pagination } from "./Pagination";
