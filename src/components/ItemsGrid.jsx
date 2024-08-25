257d2d8d2d8f05ecbffc5e3b4bb63a7cf1c12e3c
OdilovMM <dssrinman@gmail.com>
2024-08-25 23:08:32 +0500

    image url modify3

--- src/components/ItemsGrid.jsx
+++ src/components/ItemsGrid.jsx
@@ -29,7 +29,7 @@ const ItemsGrid = () => {
           >
             <figure>
               <img
-                src={`https://lost-and-found-api-r3ku.onrender.com${photo}`}
+                src={`https://lost-and-found-api-r3ku.onrender.com${photo.startsWith('/') ? photo : `/${photo}`}`}
                 alt={name}
                 className=" h-64 md:h-48 w-full object-cover"
               />
